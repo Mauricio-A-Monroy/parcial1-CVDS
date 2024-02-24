ebfe4ce66125090ae2b6c7b8599271564bde3c50
unknown <mauricio.monroy-j@labinfo.is.escuelaing.edu.co>
2024-02-24 09:04:36 -0500

    commit

--- src/main/java/edu/eci/cvds/tdd/registry/AgeExceptions.java
+++
@@ -1,8 +0,0 @@
-package edu.eci.cvds.tdd.registry;
-
-public class AgeExceptions extends Exception{
-    public static String ageInvalid = "The age is invalid";
-    public AgeExceptions(String message) {
-        super(message);
-    }
-}
